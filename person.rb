c2baab2ba7753ac7854af8ed0a241020325a96b8
bushmusi <bushera.mestofa@gmail.com>
2022-05-19 17:27:24 +0300

    fix linter

--- person.rb
+++ person.rb
@@ -2,7 +2,7 @@
 class Person
   attr_accessor  :name, :age
   attr_reader :id
-  def initialize(name="unknown", age, parent_permission=true)
+  def initialize(name="unknown", age, parent_permission: true)
     @id = Random.rand(0..1000)
     @name =name
     @age = age
@@ -12,8 +12,8 @@ class Person
   def is_of_age
     if @age >= 18
       true
-    else 
-      false 
+    else
+      false
     end
   end
 
