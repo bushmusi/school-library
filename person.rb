8ccb95ccc7e4d39969c1c7669b255a7b6dfc2f52
Bushra Mustofa <bushera.mestofa@gmail.com>
2022-05-20 22:26:10 +0300

    Update association

--- person.rb
+++ person.rb
@@ -3,7 +3,7 @@ require './nameable'
 class Person < Nameable
   attr_accessor :name, :age
 
-  attr_reader :id
+  attr_reader :id, :rentals
 
   def initialize(age, name = 'unknown', parent_permission: true)
     super()
@@ -11,6 +11,7 @@ class Person < Nameable
     @name = name
     @age = age
     @parent_permission = parent_permission
+    @rentals = []
   end
 
   def of_age
@@ -28,4 +29,9 @@ class Person < Nameable
   def correct_name
     @name
   end
+
+  def add_rental(date, book)
+    Rental.new(date, self, book)
+  end
+  
 end
--- student.rb
+++ student.rb
@@ -1,12 +1,21 @@
 require './person'
 
 class Student < Person
+  attr_reader :classroom
+  
   def initialize(classroom)
     super(age, name, parent_permission)
-    @classroom = classroom
+    self.classroom = classroom
   end
 
   def play_hooky
     "¯\(ツ)/¯"
+  end
+
+  def classroom=(new_classroom)
+    @classroom.remove_student(self) unless @classroom.nil? || @classroom == new_classroom
+
+    @classroom = new_classroom
+    @classroom.add_student(self) unless @classroom.students.include?(self)
   end
 end
