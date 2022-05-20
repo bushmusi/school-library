29d0e7389fb12f3c3dcca494ace65338da4401d1
bushmusi <bushera.mestofa@gmail.com>
2022-05-20 17:19:31 +0300

    Fix linter error

--- action.rb
+++ action.rb
@@ -4,7 +4,7 @@ require './trimmer_decorator'
 
 person = Person.new(22, 'maximilianus')
 person.correct_name
-capitalizedPerson = CapitalizeDecorator.new(person)
-capitalizedPerson.correct_name
-capitalizedTrimmedPerson = TrimmerDecorator.new(capitalizedPerson)
-capitalizedTrimmedPerson.correct_name
+Capitalized_person = CapitalizeDecorator.new(person)
+Capitalized_person.correct_name
+Capitalized_trimmedPerson = TrimmerDecorator.new(capitalizedPerson)
+Capitalized_trimmedPerson.correct_name
